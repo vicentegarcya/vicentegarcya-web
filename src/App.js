61f8fa7a10e05c35821f61e2b1c52a0ee6670356
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-07-04 21:21:28 +0200

    finish nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+const textArray = [
+  "frontend developer",
+  "tech writer",
+  "chess player",
+  "pokémon trainer",
+  "books lover"
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul className="left-side-bar">
+            <li><a href="mailto:vicentegarciaayllon@gmail.com" rel="noreferrer" target="_blank"><img src={'../images/correo-electronico.png'} alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/vicentegarciaayllon/" rel="noreferrer" target="_blank"><img src={'../images/linkedin-logo.png'} alt="" /></a></li>
+            <li><a href="https://github.com/vicentegarcya" rel="noreferrer" target="_blank"><img src={'../images/github.png'} alt="" /></a></li>
+            <li><a href="https://medium.com/@vicentegarcya" rel="noreferrer" target="_blank"><img src={'../images/medium.png'} alt="" /></a></li>
+          </ul>
+        </nav>
       </header>
+      <section className="welcome">
+        <div>
+          <img src={"../images/IMG_9606.jpg"} alt="" />
+          <div className="welcome-text">
+            <p>Hi there. I'm Vicente,</p>
+            <p>
+              {
+                textArray.map(el => el)
+              } 
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
