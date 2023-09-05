e3f89a0d934988f985a2abf60a475fb66b7d2797
Vicente Garcia <vicentegarciaayllon@gmail.com>
2023-09-05 21:22:28 +0200

    finish header animation and start about section

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import "./Home.css";
+import SectionTitle from "./components/SectionTitle/SectionTitle";
+import { Link } from "react-router-dom";
 
 function Home() {
   const aboutSection = useRef();
@@ -52,6 +54,33 @@ function Home() {
         </div>
       </section>
       <section ref={aboutSection} className="about">
+        <SectionTitle title="About"></SectionTitle>
+        <div className="about_main">
+          <div className="about_text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              facilisis elit non dui consequat convallis. Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit.
+            </p>
+            <p>
+              Sed lorem diam, porta vitae blandit id, iaculis a libero. Mauris
+              maximus vestibulum turpis eget efficitur. Vivamus tempor quam id
+              nisl mattis molestie.
+            </p>
+            <p>
+              Sed lacinia, justo et molestie finibus, eros dui tristique metus,
+              at commodo felis augue ut eros.
+            </p>
+          </div>
+          <div className="about_bubbles">
+            <Link to={"/bitacora"}>Mis textos</Link>
+            <a href="https://savee.it/vicentegarcya/" target="_blank" rel="noreferrer">Inspo</a>
+            <a href="https://mailchi.mp/cad3bc2e7c34/despertar-creativo" target="_blank" rel="noreferrer">Newsletter Fav</a>
+            <a href="https://open.spotify.com/playlist/5GWpa19ufMdY94SDjBQwA7?si=1d868ff17cc2468e" target="_blank" rel="noreferrer">My Playlist</a>
+            <a href="https://www.disney.es/peliculas/soul" target="_blank" rel="noreferrer">Movie Fav</a>
+            <a href="https://www.britannica.com/plant/daisy" target="_blank" rel="noreferrer">My Flower</a>
+          </div>
+        </div>
       </section>
     </div>
   );
---
+++ src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,9 @@
+import './SectionTitle.css';
+
+export default function SectionTitle({title}) {
+    return (
+        <div className='SectionTitle'>
+            <p>{title}.</p>
+        </div>
+    )
+}
