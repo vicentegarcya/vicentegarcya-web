0769b4a65eb0c5543f6c3f48bfcb496ef2346c07
Vicente Garcia <vicentegarciaayllon@gmail.com>
2023-09-27 17:00:02 +0200

    new try

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-import "../src/fonts/NeueMontreal/NeueMontreal-Regular.otf";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
